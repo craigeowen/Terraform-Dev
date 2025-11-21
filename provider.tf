987fc741cb55b2ed26039f099efdd3f3595584b6
Craig Owen <craigeowen@gmail.com>
2025-11-21 16:46:46 +0000

    init

---
+++ provider.tf
@@ -0,0 +1,12 @@
+provider "nxos" {
+  alias = "switch1a"
+  username = "cisco"
+  password = "cisco"
+  url      = "https://192.168.1.32"
+}
+provider "nxos" {
+  alias = "switch1b"
+  username = "cisco"
+  password = "cisco"
+  url      = "https://192.168.1.106"
+}
