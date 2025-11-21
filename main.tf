0896618fd485bea575f7d0c02f6392020fe36eb8
Craig Owen <craigeowen@gmail.com>
2025-11-21 17:04:17 +0000

    Add variables and resources for switch hostnames

--- main.tf
+++ main.tf
@@ -5,3 +5,24 @@ terraform {
     }
   }
 }
+
+variable "switch_hostname-sw1a" {
+  type    = string
+  default = "Switch-1A"
+}     
+variable "switch_hostname-sw1b" {
+  type    = string
+  default = "Switch-1B"
+} 
+
+
+
+################## HOSTNAME ###############
+resource "nxos_system" "hostname-switch1a" {
+  provider = nxos.switch1a
+  name = "${var.switch_hostname}"
+}
+resource "nxos_system" "hostname-switch1b" {
+  provider = nxos.switch1b
+  name = "${var.switch_hostname-sw1b}"
+}
