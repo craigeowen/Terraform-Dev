ad82a8a3ea9ae79a8f8864469bf771bb22fe2c3d
Craig Owen <craigeowen@gmail.com>
2025-11-25 12:01:37 +0000

    Module-Switch1a - 1

--- main.tf
+++ main.tf
@@ -18,10 +18,10 @@ module "config-SW1a" {
 }
 
 ################## HOSTNAME ###############
-resource "nxos_system" "hostname-switch1a" {
-  provider = nxos.switch1a
-  name = "${var.switch_hostname-sw1a}"
-}
+#resource "nxos_system" "hostname-switch1a" {
+#  provider = nxos.switch1a
+#  name = "${var.switch_hostname-sw1a}"
+#}
 resource "nxos_system" "hostname-switch1b" {
   provider = nxos.switch1b
   name = "${var.switch_hostname-sw1b}"
